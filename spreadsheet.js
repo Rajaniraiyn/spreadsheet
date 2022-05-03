fd5b7d3aaaa73b35a1b74cdb9fae94bd573cfe98
Teejaswan <teejaswan12@gmail.com>
2022-05-03 22:32:21 +0530

    added styles

--- spreadsheet.js
+++ spreadsheet.js
@@ -7,58 +7,6 @@ function Age(dateOfBirth) {
     var year = age_dt.getUTCFullYear();
     var a = Math.abs(year - 1970);
     return a;
-<<<<<<< HEAD
-    }
-
-function Click(index){
-  
-  let dataarray = JSON.parse(localStorage.getItem("entry"));
-  
-  if(dataarray == null){
-      let data = [];
-      data.push({name : $("name"+index).value,
-                  regNo : $("regNo"+index).value,
-                  dob: $("dob"+index).value,
-                  age: Age($("dob"+index).value),
-                  mark1: $("mark1"+index).value,
-                  mark2: $("mark2"+index).value,
-                  total: parseFloat(parseFloat($("mark1"+index).value) + parseFloat($("mark2"+index).value))
-      });
-    
-    let dataarr = data.filter(element => {
-        return element !== null;
-    }); 
-       localStorage.setItem("entry",JSON.stringify(dataarr));
-       $("name"+ index).value = $("regNo"+ index).value = $("dob"+index).value = $("age"+index).value =  $("mark1"+index).value = $("mark2"+index).value = $("total"+index).value = "";
-
-}
-else{
-    
-    
-    if($("name"+ index).value && $("regNo"+ index).value && $("dob"+index).value && $("mark1"+index).value &&  $("mark2"+index).value){
-      
-       
-       dataarray[index] = { name: $("name"+ index).value,
-       regNo: $("regNo"+ index).value,
-       dob : $("dob"+index).value,
-       age: Age($("dob"+index).value),
-       mark1: $("mark1"+index).value,
-       mark2: $("mark2"+index).value,
-       total: $("mark1"+index).value + $("mark2"+index).value
-       };
-    let dataarr = dataarray.filter(element => {
-        return element !== null;
-    });    
-    localStorage.setItem("entry",JSON.stringify(dataarr));
-   $("name"+ index).value = $("regNo"+ index).value = $("dob"+index).value = $("age"+index).innerHTML =  $("mark1"+index).value = $("mark2"+index).value = $("total"+index).innerHTML = "";
-               
-    
-    }
-    else{
-    alert("Fill all the fields!");
-    }
-=======
->>>>>>> 119ed49547693e978b880d877ed5b2c5c0c738d8
 }
 
 function Click(index) {
@@ -69,15 +17,16 @@ function Click(index) {
 
     if (dataarray == null) {
         let data = [];
-        data.push({
+        data[index] = {
             name: $("name" + index).value,
             regNo: $("regNo" + index).value,
             dob: $("dob" + index).value,
             age: Age($("dob" + index).value),
             mark1: $("mark1" + index).value,
             mark2: $("mark2" + index).value,
-            total: $("mark1" + index).value + $("mark2" + index).value
-        });
+            total: $("mark1" + index).value + $("mark2" + index).value,
+            i: index
+        };
 
         let dataarr = data.filter(element => {
             return element !== null;
@@ -99,13 +48,14 @@ function Click(index) {
                 age: Age($("dob" + index).value),
                 mark1: $("mark1" + index).value,
                 mark2: $("mark2" + index).value,
-                total: +$("mark1" + index).value + +$("mark2" + index).value
+                total: +$("mark1" + index).value + +$("mark2" + index).value,
+                i: index
             };
             let dataarr = dataarray.filter(element => {
                 return element !== null;
             });
             localStorage.setItem("entry", JSON.stringify(dataarr));
-            $("name" + index).value = $("regNo" + index).value = $("dob" + index).value = $("age" + index).innerHTML = $("mark1" + index).value = $("mark2" + index).value = $("total" + index).innerHTML = "";
+            $("name" + index).value = $("regNo" + index).value = $("dob" + index).value = $("age" + index).value = $("mark1" + index).value = $("mark2" + index).value = $("total" + index).value = " ";
 
 
         }
@@ -118,28 +68,27 @@ function Click(index) {
     Display();
 }
 function Delete(index) {
-    $("name" + index).value = $("regNo" + index).value = $("dob" + index).value = $("age" + index).innerHTML = $("mark1" + index).value = $("mark2" + index).value = $("total" + index).innerHTML = "";
+    $("name" + index).value = $("regNo" + index).value = $("dob" + index).value = $("age" + index).value = $("mark1" + index).value = $("mark2" + index).value = $("total" + index).value = " ";
     let dataarray = JSON.parse(localStorage.getItem("entry"));
     dataarray.splice(index, 1);
     localStorage.setItem("entry", JSON.stringify(dataarray));
-
+    Display();
 }
 
 function Display() {
 
     let dataarray = JSON.parse(localStorage.getItem("entry"));
-    let dataarr = dataarray.filter(element => {
-        return element !== null;
+    if (dataarray != null) {
+    dataarray.forEach((element, i) => {
+        $("name" + element.i).value = element.name;
+        $("regNo" + element.i).value = element.regNo;
+        $("dob" + element.i).value = element.dob;
+        $("age" + element.i).value = element.age;
+        $("mark1" + element.i).value = element.mark1;
+        $("mark2" + element.i).value = element.mark2;
+        $("total" + element.i).value = element.total;
     });
-    dataarr.forEach((element, i) => {
-        $("name" + i).value = element.name;
-        $("regNo" + i).value = element.regNo;
-        $("dob" + i).value = element.dob;
-        $("age" + i).innerHTML = element.age;
-        $("mark1" + i).value = element.mark1;
-        $("mark2" + i).value = element.mark2;
-        $("total" + i).innerHTML = element.total;
-    });
+}
 }
 
 function onEnter({ key, target }, control) {
@@ -147,4 +96,8 @@ function onEnter({ key, target }, control) {
         if (control.includes("del")) target.click();
         else $(control).focus();
     }
+}
+for(let i =0;i<50;i++){
+    document.getElementById("age"+i).readOnly = true;
+    document.getElementById("total"+i).readOnly = true;
 }
